87aa291d3b0014fa50ef9a0311676ee406de005c
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-10-25 18:07:25 +0600

    url generated

--- index.js
+++ index.js
@@ -17,3 +17,14 @@ const courses = require("./courses.json")
 app.get('/courses', (req, res) => {
     res.send(courses)
 })
+
+app.get('/catagory/:id', (req, res) => {
+    const id = req.params.id
+    const course = courses.find(course => course.catagory_id === id)
+    res.send(course)
+})
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id
+    const selectedCourse = courses.map(course => course.courses.find(each => each.id === id))
+    res.send(selectedCourse)
+})
