6a82b0f162d878207c3530c79bc1c25861ab05e1
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-10-25 16:36:11 +0600

    courses data and url test

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ const port = process.env.Port || 5000
 app.listen(port, () => {
     console.log("DW server is runninng on port", port)
 })
+
+app.get('/', (req, res) => {
+    res.send("E-learning Sever is running")
+})
+
+const courses = require("./courses.json")
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
